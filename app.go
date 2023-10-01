1832af4d5c421bcc1f39e02d785c28f0f7d1995c
DarVoid <jorgemduque@hotmail.com>
2023-10-01 20:54:27 +0100

    [add] bugs

--- app.go
+++ app.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -24,11 +25,13 @@ func main() {
 	r.HandleFunc("/faq", handleFAQ).Methods("GET")
 
 	r.HandleFunc("/content", handleMasterContent).Methods("GET")
-	r.HandleFunc("/experience", handleExperience).Methods("GET")
+	r.HandleFunc("/experience-template", handleExperienceTemplate).Methods("GET")
+	r.HandleFunc("/education-template", handleEducationTemplate).Methods("GET")
 
 	r.HandleFunc("/footer", handleFooter).Methods("GET")
 
 	r.HandleFunc("/icons/{icon}", handleIcons).Methods("GET")
+	r.HandleFunc("/pic/{pic}", handlePic).Methods("GET")
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./static/"))))
 
 	http.ListenAndServe(":8080", r)
@@ -49,18 +52,153 @@ func handleDefault(resp http.ResponseWriter, req *http.Request) {
 	}
 
 }
-func handleExperience(resp http.ResponseWriter, req *http.Request) {
-	filePath := "components/experience.html" // The file you want to read
 
-	// Read the contents of the file
-	contentBytes, err := os.ReadFile(filePath)
+type Entries struct {
+	De          string
+	Ate         string
+	JobTitle    string
+	Company     string
+	Where       string
+	WhereLink   string
+	Description []string //TODO: fazer um componente para dar handle desta lista
+	Stack       string
+}
+
+func handleExperienceTemplate(resp http.ResponseWriter, req *http.Request) {
+	experienceTemplate := "components/experience-template.html" // The file you want to read
+	// exeperienceTemplate := "components/descriptions.html"        // The file you want to read
+	parentComponent := "components/table.html" // The file you want to read
+
+	var total []string
+	entries := []Entries{
+		{
+			De:  "3/2022",
+			Ate: "Present",
+			Description: []string{
+				"Developed, tested, and maintained a web App for Infrastructure as Code.",
+				"Created and managed GitHub actions and reusable workflows to ensure code quality.",
+				"Collaborated with Product Owner and Stakeholders to define project vision and roadmap.",
+				// "Assumed the role of scrum master (scrum knight) and developer, enabling the delivery of value and ensuring scrum events.",
+			},
+			JobTitle:  "Fullstack Developer",
+			Company:   "Critical Techworks",
+			Where:     "Porto",
+			WhereLink: "https://goo.gl/maps/pBKxevEVCuREvH3y8",
+			Stack:     "GraphQL, Golang, NodeJS, React, Python, AWS + Terraform, DynamoDB, Mongo, Kubernetes, React, Docker, Linux, GitHub",
+		},
+		{
+			De:  "10/2022",
+			Ate: "02/2023",
+			Description: []string{
+				"Developed event-driven micro-services to support a loan Web app for Portugal and Spain markets.",
+			},
+			JobTitle:  "Backend Developer",
+			Company:   "CapGemini",
+			Where:     "Lisbon",
+			WhereLink: "https://bit.ly/3DUpYy6",
+			Stack:     "Golang, Postgres, RabbitMQ, Docker, GCP, Kubernetes, Travis",
+		},
+		{
+			De:  "04/2021",
+			Ate: "10/2022",
+			Description: []string{
+				"Planned and developed an app to manage Industrial Property.",
+				"Built CI/CD pipelines and containers for application deployment.",
+			},
+			JobTitle:  "Fullstack Developer",
+			Company:   "Techframe, SA",
+			Where:     "Abrantes",
+			WhereLink: "https://bit.ly/3n53FNo",
+			Stack:     "Angular, Docker, Golang",
+		},
+		{
+			De:  "07/2020",
+			Ate: "03/2021",
+			Description: []string{
+				"Created ETL scripts and refactored APIs for internal frontend applications.",
+			},
+			JobTitle:  "Fullstack Developer",
+			Company:   "Softinsa, IBM",
+			Where:     "Tomar",
+			WhereLink: "https://bit.ly/35vZgeX",
+			Stack:     "T-SQL, Node.js, Python",
+		},
+	}
+
+	contentBytes, err := os.ReadFile(experienceTemplate)
 	if err != nil {
 		fmt.Println("Error reading the file:", err)
 	}
-	content := string(contentBytes)
+	formatString := string(contentBytes)
+	for _, entry := range entries {
+		total = append(total,
+			fmt.Sprintf(formatString,
+				entry.De,
+				entry.Ate,
+				entry.JobTitle,
+				entry.Company,
+				entry.WhereLink,
+				entry.Where,
+				entry.Description,
+				entry.Stack))
+	}
+	contentBytes, err = os.ReadFile(parentComponent)
+	if err != nil {
+		fmt.Println("Error reading the file:", err)
+	}
+	formatString = string(contentBytes)
+	resposta := fmt.Sprintf(formatString, "Experience", strings.Join(total, "\n"))
 
 	if req.Method == "GET" {
-		fmt.Fprintf(resp, "%v", content)
+		fmt.Fprintf(resp, "%v", resposta)
+	}
+
+}
+func handleEducationTemplate(resp http.ResponseWriter, req *http.Request) {
+	experienceTemplate := "components/education-template.html" // The file you want to read
+	// exeperienceTemplate := "components/descriptions.html"        // The file you want to read
+	parentComponent := "components/table.html" // The file you want to read
+
+	var total []string
+	entries := []Entries{
+		{
+			De:          "2017",
+			Ate:         "2020",
+			Description: []string{"cenas"},
+			JobTitle:    "Computer Engineering",
+			Company:     "Bachelor's Degree",
+			Where:       "IPT",
+			WhereLink:   "https://bit.ly/2IsX53k",
+			Stack:       "GraphQL, Golang, NodeJS, React, Python, AWS + Terraform, DynamoDB, Mongo, Kubernetes, React, Docker, Linux, GitHub",
+		},
+	}
+
+	contentBytes, err := os.ReadFile(experienceTemplate)
+	if err != nil {
+		fmt.Println("Error reading the file:", err)
+	}
+	formatString := string(contentBytes)
+	for _, entry := range entries {
+		total = append(total,
+			fmt.Sprintf(formatString,
+				entry.De,
+				entry.Ate,
+				entry.JobTitle,
+				entry.Company,
+				entry.WhereLink,
+				entry.Where,
+				entry.Description,
+				entry.Stack))
+	}
+	contentBytes, err = os.ReadFile(parentComponent)
+	if err != nil {
+		fmt.Println("Error reading the file:", err)
+	}
+	formatString = string(contentBytes)
+	resposta := fmt.Sprintf(formatString, "Education", strings.Join(total, "\n"))
+
+	if req.Method == "GET" {
+		fmt.Fprintf(resp, "%v", resposta)
 	}
 
 }
@@ -155,21 +293,6 @@ func handleFooter(resp http.ResponseWriter, req *http.Request) {
 }
 func handleLeftPane(resp http.ResponseWriter, req *http.Request) {
 	filePath := "components/master-left-pane.html" // The file you want to read
-
-	// Read the contents of the file
-	contentBytes, err := os.ReadFile(filePath)
-	if err != nil {
-		fmt.Println("Error reading the file:", err)
-	}
-	content := string(contentBytes)
-
-	if req.Method == "GET" {
-		fmt.Fprintf(resp, "%v", content)
-	}
-}
-
-func handleLinuxIcon(resp http.ResponseWriter, req *http.Request) {
-	filePath := "icons/linux-icon.svg" // The file you want to read
 
 	// Read the contents of the file
 	contentBytes, err := os.ReadFile(filePath)
--- icons.go
+++ icons.go
@@ -24,3 +24,19 @@ func handleIcons(resp http.ResponseWriter, req *http.Request) {
 		fmt.Fprintf(resp, "%v", content)
 	}
 }
+func handlePic(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	pic := vars["pic"]
+	filePath := fmt.Sprintf("components/pic%v.html", pic) // The file you want to read
+
+	// Read the contents of the file
+	contentBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Println("Error reading the file:", err)
+	}
+	content := string(contentBytes)
+
+	if req.Method == "GET" {
+		fmt.Fprintf(resp, "%v", content)
+	}
+}
