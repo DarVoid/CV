2200014d8c6c924dc81d03360e9160273a587930
DarVoid <jorgemduque@hotmail.com>
2023-10-06 17:45:17 +0100

    waaaaay better

--- experience.go
+++ experience.go
@@ -1,251 +1,151 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
+	"text/template"
 )
 
 // TODO: fazer um componente para dar handle desta lista
 type Entries struct {
-	De          string
-	Ate         string
-	JobTitle    string
-	Company     string
-	Where       string
-	WhereLink   string
-	Description []string
-	Stack       string
+	De          string   `json:"De"`
+	Ate         string   `json:"Ate"`
+	JobTitle    string   `json:"JobTitle"`
+	Company     string   `json:"Company"`
+	Where       string   `json:"Where"`
+	WhereLink   string   `json:"WhereLink"`
+	Description []string `json:"Description"`
+	Stack       string   `json:"Stack"`
+}
+type EntriesTemplatable struct {
+	De          string   
+	Ate         string   
+	JobTitle    string   
+	Company     string   
+	Where       string   
+	WhereLink   string   
+	Description string 
+	Stack       string   
 }
 
 func handleExperienceTemplate(resp http.ResponseWriter, req *http.Request) {
-	experienceTemplate := "components/experience-template.html" // The file you want to read
 
-	var total []string
-	entries := []Entries{
-		{
-			De:  "03/2022",
-			Ate: "Present",
-			Description: []string{
-				"Developed, tested, and maintained a web App for Infrastructure as Code.",
-				"Created and managed GitHub actions and reusable workflows to ensure code quality.",
-				"Collaborated with Product Owner and Stakeholders to define project vision and roadmap.",
-			},
-			// "Assumed the role of scrum master (scrum knight) and developer, enabling the delivery of value and ensuring scrum events.",
-			JobTitle:  "Fullstack Developer",
-			Company:   "Critical Techworks",
-			Where:     "Porto",
-			WhereLink: "https://goo.gl/maps/pBKxevEVCuREvH3y8",
-			Stack:     "GraphQL, Golang, NodeJS, React, Python, AWS + Terraform, DynamoDB, Mongo, Kubernetes, React, Docker, Linux, GitHub",
-		},
-		{
-			De:  "10/2022",
-			Ate: "02/2023",
-			Description: []string{
-				"Developed event-driven micro-services to support a loan Web app for Portugal and Spain markets.",
-			},
-			JobTitle:  "Backend Developer",
-			Company:   "CapGemini",
-			Where:     "Lisbon",
-			WhereLink: "https://bit.ly/3DUpYy6",
-			Stack:     "Golang, Postgres, RabbitMQ, Docker, GCP, Kubernetes, Travis",
-		},
-		{
-			De:  "04/2021",
-			Ate: "10/2022",
-			Description: []string{
-				"Planned and developed an app to manage Industrial Property.",
-				"Built CI/CD pipelines and containers for application deployment.",
-			},
-			JobTitle:  "Fullstack Developer",
-			Company:   "Techframe, SA",
-			Where:     "Abrantes",
-			WhereLink: "https://bit.ly/3n53FNo",
-			Stack:     "Angular, Docker, Golang",
-		},
-		{
-			De:  "07/2020",
-			Ate: "03/2021",
-			Description: []string{
-				"Created ETL scripts and refactored APIs for internal frontend applications.",
-			},
-			JobTitle:  "Fullstack Developer",
-			Company:   "Softinsa, IBM",
-			Where:     "Tomar",
-			WhereLink: "https://bit.ly/35vZgeX",
-			Stack:     "T-SQL, Node.js, Python",
-		},
-	}
+	component := getComponent("components/eduexperience-template.html") // The file you want to read
+	var entries []Entries
+	var templatableEntries []EntriesTemplatable
+	
+	getEntries("data/experience.json", &entries)
 
-	contentBytes, err := os.ReadFile(experienceTemplate)
+	getTemplatable(entries, &templatableEntries)
+	
+
+	var buf bytes.Buffer
+
+	t, err := template.New("experience").Parse(component)
 	if err != nil {
-		fmt.Println("Error reading the file:", err)
+		fmt.Println(err)
 	}
-	formatString := string(contentBytes)
-	for _, entry := range entries {
-		resolvedList := []string{}
-		for _, item := range entry.Description {
-			resolvedList = append(resolvedList, descriptionResolveComponent("components/description-template.html", item))
+	
+
+	for _, entry := range templatableEntries {
+		
+		err = t.ExecuteTemplate(&buf, "experience", entry)
+		if err != nil {
+			fmt.Println(err)
 		}
-		container := descriptionResolveComponent("components/description-template-container.html", strings.Join(resolvedList, "\n"))
-		total = append(total,
-			fmt.Sprintf(formatString,
-				entry.De,
-				entry.Ate,
-				entry.JobTitle,
-				entry.Company,
-				entry.WhereLink,
-				entry.Where,
-				container,
-				entry.Stack))
 	}
+
 	if req.Method == "GET" {
-		fmt.Fprintf(resp, "%v", strings.Join(total, "\n"))
+		fmt.Fprintf(resp, "%v", &buf)
 	}
 
-}
-func handleExperienceTemplateGo(resp http.ResponseWriter, req *http.Request) {
-	experienceTemplate := "components/experience-template.html" // The file you want to read
-
-	var total []string
-	entries := []Entries{
-		{
-			De:  "03/2022",
-			Ate: "Present",
-			Description: []string{
-				"Developed, tested, and maintained a web App for Infrastructure as Code.",
-				"Created and managed GitHub actions and reusable workflows to ensure code quality.",
-				"Collaborated with Product Owner and Stakeholders to define project vision and roadmap.",
-			},
-			// "Assumed the role of scrum master (scrum knight) and developer, enabling the delivery of value and ensuring scrum events.",
-			JobTitle:  "Fullstack Developer",
-			Company:   "Critical Techworks",
-			Where:     "Porto",
-			WhereLink: "https://goo.gl/maps/pBKxevEVCuREvH3y8",
-			Stack:     "GraphQL, Golang, NodeJS, React, Python, AWS + Terraform, DynamoDB, Mongo, Kubernetes, React, Docker, Linux, GitHub",
-		},
-		{
-			De:  "10/2022",
-			Ate: "02/2023",
-			Description: []string{
-				"Developed event-driven micro-services to support a loan Web app for Portugal and Spain markets.",
-			},
-			JobTitle:  "Backend Developer",
-			Company:   "CapGemini",
-			Where:     "Lisbon",
-			WhereLink: "https://bit.ly/3DUpYy6",
-			Stack:     "Golang, Postgres, RabbitMQ, Docker, GCP, Kubernetes, Travis",
-		},
-		{
-			De:  "04/2021",
-			Ate: "10/2022",
-			Description: []string{
-				"Planned and developed an app to manage Industrial Property.",
-				"Built CI/CD pipelines and containers for application deployment.",
-			},
-			JobTitle:  "Fullstack Developer",
-			Company:   "Techframe, SA",
-			Where:     "Abrantes",
-			WhereLink: "https://bit.ly/3n53FNo",
-			Stack:     "Angular, Docker, Golang",
-		},
-		{
-			De:  "07/2020",
-			Ate: "03/2021",
-			Description: []string{
-				"Created ETL scripts and refactored APIs for internal frontend applications.",
-			},
-			JobTitle:  "Fullstack Developer",
-			Company:   "Softinsa, IBM",
-			Where:     "Tomar",
-			WhereLink: "https://bit.ly/35vZgeX",
-			Stack:     "T-SQL, Node.js, Python",
-		},
-	}
-
-	contentBytes, err := os.ReadFile(experienceTemplate)
-	if err != nil {
-		fmt.Println("Error reading the file:", err)
-	}
-	formatString := string(contentBytes)
-	for _, entry := range entries {
-		resolvedList := []string{}
-		for _, item := range entry.Description {
-			resolvedList = append(resolvedList, descriptionResolveComponent("components/description-template.html", item))
-		}
-		container := descriptionResolveComponent("components/description-template-container.html", strings.Join(resolvedList, "\n"))
-		total = append(total,
-			fmt.Sprintf(formatString,
-				entry.De,
-				entry.Ate,
-				entry.JobTitle,
-				entry.Company,
-				entry.WhereLink,
-				entry.Where,
-				container,
-				entry.Stack))
-	}
-	if req.Method == "GET" {
-		fmt.Fprintf(resp, "%v", strings.Join(total, "\n"))
-	}
 
 }
 func handleEducationTemplate(resp http.ResponseWriter, req *http.Request) {
-	experienceTemplate := "components/education-template.html" // The file you want to read
+	component := getComponent("components/eduexperience-template.html") // The file you want to read
+	var entries []Entries
+	var templatableEntries []EntriesTemplatable
+	
+	getEntries("data/education.json", &entries)
 
-	var total []string
-	entries := []Entries{
-		{
-			De:          "2017",
-			Ate:         "2020",
-			Description: []string{"cenas"},
-			JobTitle:    "Computer Engineering",
-			Company:     "Bachelor's Degree",
-			Where:       "IPT",
-			WhereLink:   "https://bit.ly/2IsX53k",
-			Stack:       "Ruby on rails,Angular, Travis, Docker, Vagrant, MatLab, Python, Java, CentOS, etc...",
-		},
+	getTemplatable(entries, &templatableEntries)
+	
+
+	var buf bytes.Buffer
+
+	t, err := template.New("education").Parse(component)
+	if err != nil {
+		fmt.Println(err)
 	}
+	
+
+	for _, entry := range templatableEntries {
+		
+		err = t.ExecuteTemplate(&buf, "education", entry)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	if req.Method == "GET" {
+		fmt.Fprintf(resp, "%v", &buf)
+	}
+
+}
+
+func getComponent(path string) string {
+	experienceTemplate := path // The file you want to read
 
 	contentBytes, err := os.ReadFile(experienceTemplate)
 	if err != nil {
 		fmt.Println("Error reading the file:", err)
 	}
-	formatString := string(contentBytes)
-	for _, entry := range entries {
-		resolvedList := []string{}
-		for _, item := range entry.Description {
-			resolvedList = append(resolvedList, descriptionResolveComponent("components/description-template.html", item))
-		}
-		container := descriptionResolveComponent("components/description-template-container.html", strings.Join(resolvedList, "\n"))
-
-		total = append(total,
-			fmt.Sprintf(formatString,
-				entry.De,
-				entry.Ate,
-				entry.JobTitle,
-				entry.Company,
-				entry.WhereLink,
-				entry.Where,
-				container,
-				entry.Stack))
-	}
-
-	if req.Method == "GET" {
-		fmt.Fprintf(resp, "%v", strings.Join(total, "\n"))
-	}
-
+	component := string(contentBytes)
+	return component
 }
 
-// utils
-
-func descriptionResolveComponent(path, value string) string {
-	contentBytes, err := os.ReadFile(path)
+func getEntries(path string, entries *[]Entries) {
+	file, err := os.Open(path)
 	if err != nil {
-		fmt.Println("Error reading the file:", err)
+		fmt.Println("Error opening JSON file:", err)
+		return
 	}
-	formatString := string(contentBytes)
+	defer file.Close()
 
-	return fmt.Sprintf(formatString, value)
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&entries)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+}
+
+func getTemplatable(entries []Entries, to *[]EntriesTemplatable)  {
+	description := getComponent("components/description-template.html") // The file you want to read
+	t2, err := template.New("description").Parse(description)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, entry := range entries {
+		var buf2 bytes.Buffer
+		for _, val := range entry.Description{
+			err := t2.ExecuteTemplate(&buf2, "description", val)
+			if err != nil{
+				fmt.Println(err)
+			}
+		}
+		*(to) = append(*(to), EntriesTemplatable{
+			De: entry.De,
+			Ate: entry.Ate,
+			JobTitle: entry.JobTitle,
+			Company: entry.Company,
+			Where: entry.Where,
+			WhereLink: entry.WhereLink,
+			Stack: entry.Stack,
+			Description: buf2.String(),
+		})
+	}
 }
